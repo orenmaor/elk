e8d647d2023f3cdfd640311c8cfb308435089464
root <root@chef.sturdynetworks.com>
2014-10-17 06:12:51 +0000

    removed marvel

--- elasticsearch/recipes/plugins.rb
+++ elasticsearch/recipes/plugins.rb
@@ -1,7 +1,7 @@
-execute "install-marvel" do
-  command "/usr/share/elasticsearch/bin/plugin -i elasticsearch/marvel/latest"
-  not_if { ::File.directory?("#{node['elasticsearch']['maven_dir']}") }
-end
+#execute "install-marvel" do
+#  command "/usr/share/elasticsearch/bin/plugin -i elasticsearch/marvel/latest"
+#  not_if { ::File.directory?("#{node['elasticsearch']['maven_dir']}") }
+#end
 
 execute "install-head" do
   command "/usr/share/elasticsearch/bin/plugin -i mobz/elasticsearch-head"
